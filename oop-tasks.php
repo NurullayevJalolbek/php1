bb3caa7950d3507cdc6371b2aeeecad44aaf93e5
Bilol <agentusbro501@gmail.com>
2026-08-24 15:33:41 +0500

    task 2

--- oop-tasks.php
+++ oop-tasks.php
@@ -22,3 +22,32 @@ $product2 = new Product();
 $product2->name = 'Mouse';
 $product2->price = 10;
 echo $product2->getInfo() . '<br>';
+
+
+
+class User 
+{
+    public function __construct(private string $name, private readonly string $email, private int $age)
+    {
+    }
+
+    public function isAdult(): bool
+    {
+        if ($this->age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+}
+
+$user1 = new User('Sardor', 'sardor007@gmail.com', 16);
+
+echo $user1->isAdult();
+
+$user2 = new User('Ali', 'ali2004@gmail.com', 22);
+
+echo $user2->isAdult();
+
+
+
